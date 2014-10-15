327b916a4585e1cdc72e6f7c7a632bd07b5a562c
german <german@germanPC.(none)>
2014-10-15 13:20:06 +0400

    "new"

---
+++ ui/src/main/java/ru/tsystems/tsproject/tproject/LoginServlet.java
@@ -0,0 +1,16 @@
+package ru.tsystems.tsproject.tproject;
+import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by german on 15.10.14.
+ */
+public class LoginServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+
+    }
+}
