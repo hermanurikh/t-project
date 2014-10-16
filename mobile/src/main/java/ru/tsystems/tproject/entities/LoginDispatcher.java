a3f57c4828ecc344359a8813b8903987b773ad6e
german <german@germanPC.(none)>
2014-10-16 08:45:06 +0400

    changed structure

---
+++ mobile/src/main/java/ru/tsystems/tproject/entities/LoginDispatcher.java
@@ -0,0 +1,11 @@
+package ru.tsystems.tproject.entities;
+
+/**
+ * Created by german on 15.10.14.
+ */
+public class LoginDispatcher {
+    public static boolean dispatch(){
+
+        return false;
+    }
+}
