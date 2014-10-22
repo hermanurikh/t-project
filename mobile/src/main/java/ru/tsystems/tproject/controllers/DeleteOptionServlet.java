3780626fdadd852c4c081c05d37d86f98dd8f01d
german <german@germanPC.(none)>
2014-10-22 10:59:13 +0400

    deletepage

--- mobile/src/main/java/ru/tsystems/tproject/controllers/DeleteOptionServlet.java
+++ mobile/src/main/java/ru/tsystems/tproject/controllers/DeleteOptionServlet.java
@@ -1,44 +1,35 @@
 package ru.tsystems.tproject.controllers;
-import org.apache.log4j.Logger;
-
-
 
 import ru.tsystems.tproject.entities.Option;
 import ru.tsystems.tproject.services.API.OptionService;
 import ru.tsystems.tproject.services.implementation.OptionServiceImplementation;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by german on 21.10.14.
  */
-public class AllOptionsServlet extends HttpServlet {
-    private static Logger logger = Logger.getLogger(AllOptionsServlet.class);
-    static List<Option> optionsList = new ArrayList<>();
+public class DeleteOptionServlet extends HttpServlet {
+    private static List<Option> optionsList;
 
-    public void doGet(HttpServletRequest request,
-                       HttpServletResponse response)
-            throws ServletException, IOException
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         response.setContentType("text/html;charset=utf-8");
         try {
+            int optionID = Integer.parseInt(String.valueOf(request.getAttribute("optionId")));
             OptionService optionService = new OptionServiceImplementation();
+            optionService.deleteOption(optionService.getOptionById(optionID));
             optionsList = optionService.getAllOptions();
             request.getSession().setAttribute("optionsList", optionsList);
-            /*RequestDispatcher rd = request.getRequestDispatcher("../cp_employee/cp_employee_options.jsp");
-            rd.forward(request, response);*/
             response.sendRedirect("../cp_employee/cp_employee_options.jsp");
         }
         catch (Exception ex) {
             response.sendRedirect("../cp_employee/exception.html");
-
         }
     }
 }
